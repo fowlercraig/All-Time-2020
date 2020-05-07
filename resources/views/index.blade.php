57e870c9ca3e59e2ca28a71b287af421c80c604e
Craig Fowler <craig@howler.la>
2020-05-07 14:28:23 -0700

    added blog

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,7 +10,7 @@
   @endif
 
   @while (have_posts()) @php the_post() @endphp
-    @include('partials.content-'.get_post_type())
+    @include('partials.content-single-'.get_post_type())
   @endwhile
 
   {!! get_the_posts_navigation() !!}
--- resources/views/partials/content-single.blade.php
+++ resources/views/partials/content-single.blade.php
@@ -1,6 +1,20 @@
 <article @php post_class() @endphp>
   <header>
-    <h2 class="entry-title font-sans text-sm uppercase tracking-widest font-medium">
+
+    <?php if ( has_post_thumbnail() ) : $caption = get_post(get_post_thumbnail_id())->post_excerpt; ?>
+    <figure class="mb-6">
+      <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
+        <?php the_post_thumbnail(); ?>
+      </a>
+      <?php if ( $caption ) : ?>
+      <figcaption class="text-xs py-2">
+        <?php echo $caption ?>
+      </figcaption>
+      <?php endif; ?>
+    </figure>
+    <?php endif; ?>
+
+    <h2 class="entry-title font-sans text-sm uppercase tracking-widest font-medium mb-6">
       <a href="{{ get_permalink() }}">{!! get_the_title() !!}</a>
     </h2>
   </header>
