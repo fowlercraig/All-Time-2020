1805cd59bd4c5f2ddd6977391f3b3b7229a4316b
Craig Fowler <craig@howler.la>
2020-08-06 15:46:31 -0700

    buttons

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,11 @@
+<div>
+  <label for="email" class="block text-sm font-medium leading-5 text-gray-700">Email</label>
+  <div class="mt-1 relative rounded-md shadow-sm">
+    <input id="email" class="form-input block w-full sm:text-sm sm:leading-5" placeholder="you@example.com">
+  </div>
+</div>
+<span class="inline-flex rounded-md shadow-sm">
+  <button type="button" class="inline-flex items-center px-4 py-2 border border-gray-300 text-sm leading-5 font-medium rounded-md text-gray-700 bg-white hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:text-gray-800 active:bg-gray-50 transition ease-in-out duration-150">
+    Button text
+  </button>
+</span>
