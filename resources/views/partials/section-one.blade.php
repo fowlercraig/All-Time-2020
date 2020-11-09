ef4a65c5e803f0ad2b3a13e486c5ff1664c03233
Craig Fowler <craig@howler.la>
2020-11-09 10:31:49 -0800

    all time

--- resources/views/partials/section-one.blade.php
+++ resources/views/partials/section-one.blade.php
@@ -2,6 +2,9 @@
 
 @if($images)
   <section class="relative">
+    <div class="absolute inset-0 z-20">
+      <img class="block w-full h-full object-contain object-center" src="@asset('images/alltime_logo.svg')" alt="{{ $siteName }}">
+    </div>
     <div class="gallery-images">
       @foreach($images as $image)
       <div class="gallery-image h-70vh md:h-80vh bg-black">
